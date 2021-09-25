81c52dae92bae62ad734a98d2573fb4e3c0f39e3
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2021-09-25 08:30:19 +0100

    add showModal eventlistener

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ const showModal = document.querySelectorAll('.show-modal')
 const btnCloseModal = document.querySelector('.close-modal')
 const overlay = document.querySelector('.overlay')
 
+for (let i = 0; i < showModal.length; i++) {
+  showModal[i].addEventListener('click',() => {
+    modal.classList.remove('hidden')
+    overlay.classList.remove('hidden')
+  
+    // OR
+  
+    // modal.style.display = 'block'
+    // overlay.style.display = 'block'
+  })
+  }
