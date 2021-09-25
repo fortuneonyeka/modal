e506ae0e7444e2fd1d7f19efc0da8d13455e960e
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2021-09-25 08:38:19 +0100

    add openModal function

--- script.js
+++ script.js
@@ -4,15 +4,19 @@ const showModal = document.querySelectorAll('.show-modal')
 const btnCloseModal = document.querySelector('.close-modal')
 const overlay = document.querySelector('.overlay')
 
-for (let i = 0; i < showModal.length; i++) {
-  showModal[i].addEventListener('click',() => {
-    modal.classList.remove('hidden')
-    overlay.classList.remove('hidden')
-  
-    // OR
+const openModal = () => {
+  modal.classList.remove('hidden')
+  overlay.classList.remove('hidden')
+
+  // OR
   
     // modal.style.display = 'block'
     // overlay.style.display = 'block'
+}
+
+for (let i = 0; i < showModal.length; i++) {
+  showModal[i].addEventListener('click',() => {
+    openModal()
   })
   }
 
