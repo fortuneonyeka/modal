69b5ae068d34f12501449c904af25f1d277d93dc
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2021-09-25 08:29:14 +0100

    add js variable in the DOM

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 'use strict';
+const modal = document.querySelector('.modal');
+const showModal = document.querySelectorAll('.show-modal')
+const btnCloseModal = document.querySelector('.close-modal')
+const overlay = document.querySelector('.overlay')
+
